338ee67f994baf4cbdc14dc01ef238927853e6ba
Goncalo Pinto <fc58178@alunos.fc.ul.pt>
2024-03-08 12:54:07 +0000

    Construtor da classe Domain e alterações no server

--- Domain.java
+++ Domain.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Domain {
+
+    String domainName;
+    String Owner;
+    ArrayList<String> userList = new ArrayList<>();
+    HashMap<String, ArrayList<Integer>> devicesList = new HashMap<>();
     
+    public Domain (String Owner, String domainName) {
+        this.Owner = Owner;
+        this.domainName = domainName;
+    }
+
 }
